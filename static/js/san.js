ac21e8bbc9dc713ef6aa974653d09e0c97ecdda6
Jorge Romero <jorge.romero@sanwise.net>
2024-07-23 20:09:44 -0500

    HTML Form with google Sheets

--- static/js/san.js
+++ static/js/san.js
@@ -1,1 +1,16 @@
 
+var form = document.getElementById("sanwise-form-submit");
+                  form.addEventListener("submit", (e) => {
+                    e.preventDefault();
+                    fetch(form.action, {
+                      method: "POST",
+                      body: new FormData(
+                        document.getElementById("sanwise-form-submit")
+                      ),
+                    })
+                    .then((response) => response.json())
+                    .then((html) => {
+                      window.open("send_form.html", "_self");
+                    });
+
+                });
